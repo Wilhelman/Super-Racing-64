b5b7dec726c3ec3e810a1b1da23217d0aecfbb42
nintervik <vmasogarcia@gmail.com>
2018-01-03 17:39:44 +0100

    Increased cars acceleration

--- SuperRacing64/ModulePlayer.h
+++ SuperRacing64/ModulePlayer.h
@@ -5,7 +5,7 @@
 
 struct PhysVehicle3D;
 
-#define MAX_ACCELERATION 1000.0f
+#define MAX_ACCELERATION 1500.0f
 #define BACK_ACCELERATION -500.0f
 #define TURN_DEGREES 15.0f * DEGTORAD
 #define BRAKE_POWER 1000.0f
